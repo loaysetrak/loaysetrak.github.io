6b4b352fc29094babe088e3b67496b7853f110d3
loaysetrak <48985250+loaysetrak@users.noreply.github.com>
2019-06-03 21:36:02 +1000

    Add files via upload

---
+++ precache-manifest.cf17a95535bf8bd12da56b9f2e03e8ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d487d23992a6c1d560848960e41d2af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4259b7f7fe36f0eb7fd",
+    "url": "/static/css/main.5b08864d.chunk.css"
+  },
+  {
+    "revision": "36e06caee400cb8ad756",
+    "url": "/static/js/2.66474b88.chunk.js"
+  },
+  {
+    "revision": "c4259b7f7fe36f0eb7fd",
+    "url": "/static/js/main.90c26b29.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
