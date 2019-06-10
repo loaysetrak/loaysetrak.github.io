04203a78d207b5d48c2e967c2e9db67f27756683
loaysetrak <48985250+loaysetrak@users.noreply.github.com>
2019-06-10 17:13:00 +1000

    gh

--- precache-manifest.b5a0c70c4aaedf31e337cd8b7e904ad6.js
+++ precache-manifest.b5a0c70c4aaedf31e337cd8b7e904ad6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "15262a84bcaf2508c9133baefe999b21",
+    "revision": "226e064e0b6a42dbf15691e4bfd8d4dc",
     "url": "/index.html"
   },
   {
-    "revision": "209588889990d3d8a170",
+    "revision": "899faf7981a336eb24f7",
     "url": "/static/css/main.cc48f3fe.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.ff6e722a.chunk.js"
   },
   {
-    "revision": "209588889990d3d8a170",
-    "url": "/static/js/main.02c951f6.chunk.js"
+    "revision": "899faf7981a336eb24f7",
+    "url": "/static/js/main.33e26a4f.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe3a79b9e66a7ca86471c5439225702c.js"
+  "/precache-manifest.b5a0c70c4aaedf31e337cd8b7e904ad6.js"
 );
 
 self.addEventListener('message', (event) => {
