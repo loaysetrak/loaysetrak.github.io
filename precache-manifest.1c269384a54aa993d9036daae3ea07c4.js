039098212dae8fae14f58af87b9f15ccef2a506f
loaysetrak <48985250+loaysetrak@users.noreply.github.com>
2019-08-21 21:53:23 +1000

    gh

--- precache-manifest.1c269384a54aa993d9036daae3ea07c4.js
+++ precache-manifest.1c269384a54aa993d9036daae3ea07c4.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "49ff281463c2f0f4af87ab1cc73b8cba",
+    "revision": "590477d62965fb2a1f1ea43e2eb27abb",
     "url": "/index.html"
   },
   {
-    "revision": "002ebb0e2fed37478ebf",
+    "revision": "e3a57d0fc7663677dcbb",
     "url": "/static/css/main.6ee72844.chunk.css"
   },
   {
-    "revision": "13ec166f3cfbf559eb20",
-    "url": "/static/js/2.d54fe906.chunk.js"
+    "revision": "18737fbb527c32aac3d4",
+    "url": "/static/js/2.bfabbd56.chunk.js"
   },
   {
-    "revision": "002ebb0e2fed37478ebf",
-    "url": "/static/js/main.a4572958.chunk.js"
+    "revision": "e3a57d0fc7663677dcbb",
+    "url": "/static/js/main.9aec9e86.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ba4ed89f5191a94e0a9c9dae38c03331.js"
+  "/precache-manifest.1c269384a54aa993d9036daae3ea07c4.js"
 );
 
 self.addEventListener('message', (event) => {
