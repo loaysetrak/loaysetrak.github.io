6aceac3be58d5cb4a0f09f29e70b47681503e060
loaysetrak <48985250+loaysetrak@users.noreply.github.com>
2021-05-18 22:03:21 +1000

    gh

--- precache-manifest.3e32f7f610d355745c4a6004bd3af698.js
+++ precache-manifest.3e32f7f610d355745c4a6004bd3af698.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "55abbf8e75654e8c83f93259e60c9cfd",
+    "revision": "c346dd0dad3ea3b2582d48bdf76b2e2b",
     "url": "/index.html"
   },
   {
-    "revision": "8753934bf0650137256b",
+    "revision": "be6b0ab8d785b6b2e956",
     "url": "/static/css/2.7431b046.chunk.css"
   },
   {
-    "revision": "5b8cbce61968817c14c1",
+    "revision": "953909c90017f8e5019b",
     "url": "/static/css/main.35b2303b.chunk.css"
   },
   {
-    "revision": "8753934bf0650137256b",
-    "url": "/static/js/2.7fe465a0.chunk.js"
+    "revision": "be6b0ab8d785b6b2e956",
+    "url": "/static/js/2.0704961a.chunk.js"
   },
   {
-    "revision": "5b8cbce61968817c14c1",
-    "url": "/static/js/main.0f16289d.chunk.js"
+    "revision": "953909c90017f8e5019b",
+    "url": "/static/js/main.739e11d6.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f95fe2d249cffe56c21fdbb11742525.js"
+  "/precache-manifest.3e32f7f610d355745c4a6004bd3af698.js"
 );
 
 self.addEventListener('message', (event) => {
